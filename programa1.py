d23e0e646c43251d3d1d323124042184ee912372
Graciela Ramirez-Alonso <galonso@uach.mx>
2025-05-28 17:23:23 -0600

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [28.6353, -106.0889],
-    columns=["lat", "lon"],
-)
-st.map(df)
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.scatter_chart(chart_data)
